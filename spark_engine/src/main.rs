6a32c5717e1e692443a1102d870362f557844134
arcolinuxz <arcolinuxinfo@gmail.com>
2022-09-14 14:55:07 -0400

    Imports

--- spark_engine/src/main.rs
+++ spark_engine/src/main.rs
@@ -1,12 +1,14 @@
-use glium::glutin;
+use glium::glutin::{window::WindowBuilder, ContextBuilder};
+use glium::glutin::event_loop::{EventLoop};
+use glium::{Display};
 
 mod core;
 
 fn main() {
-    let event_loop = glutin::event_loop::EventLoop::new();
-    let wb = glutin::window::WindowBuilder::new();
-    let cb = glutin::ContextBuilder::new();
-    let display = match glium::Display::new(wb, cb, &event_loop) {
+    let event_loop = EventLoop::new();
+    let wb = WindowBuilder::new();
+    let cb = ContextBuilder::new();
+    let display = match Display::new(wb, cb, &event_loop) {
         Ok(result) => result,
         Err(why) => println!("Could not get display: {}", why)
     };
