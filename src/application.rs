519c307022fa0fe567ca31ab4e17b5fddda3297c
Hudson Finn <hkfinn@tidewater.net>
2023-02-07 08:54:16 -0500

    Refactor into modules

--- src/application.rs
+++ src/application.rs
@@ -11,13 +11,15 @@ pub fn run(event_loop: glutin::event_loop::EventLoop<()>, display: glium::Displa
     event_loop.run(move |ev, _, control_flow| {
         let mut target = display.draw();
         target.clear_color(color.red,  color.green, color.blue, 1.0);
+        // Draw here
+          
+        
         target.finish().unwrap();
 
         let next_frame_time = std::time::Instant::now() +
             std::time::Duration::from_nanos(16_666_667);
 
-        *control_flow = glutin::event_loop::ControlFlow::WaitUntil
-            (next_frame_time);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
         if let glutin::event::Event::WindowEvent { event, .. } = ev {
             if event == glutin::event::WindowEvent::CloseRequested {
                 *control_flow = glutin::event_loop::ControlFlow::Exit; 
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-use glium::glutin::{window::WindowBuilder, ContextBuilder};
 use glium::glutin::event_loop::EventLoop;
+use glium::glutin::{window::WindowBuilder, ContextBuilder};
 use glium::Display;
-use crate::core::{ run, Color};
-mod core;
+use spark_engine::application::{run, Color};
 
 fn main() {
     let event_loop = EventLoop::new();
     let wb = WindowBuilder::new();
     let cb = ContextBuilder::new();
     let display = Display::new(wb, cb, &event_loop).unwrap();
-    let color = Color { red: 33.0 / 255.0, green: 33.0 / 255.0, blue: 33.0 / 255.0 };
+    let color = Color {
+        red: 33.0 / 255.0,
+        green: 33.0 / 255.0,
+        blue: 33.0 / 255.0,
+    };
     run(event_loop, display, color);
 }
