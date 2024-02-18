fdcca07f6721e1b2917f3e316807d756dd0273c8
hudson <hfinn@autodist.com>
2024-02-18 10:33:38 -0500

    Shader class

--- src/main.cpp
+++ src/main.cpp
@@ -21,13 +21,12 @@ const char* fragmentShaderSource =
 	"}\0";
 
 int main() {
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
 	if(!glfwInit()) {
 		return -1;
 	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLFWwindow* window = glfwCreateWindow(800, 600, "Spark", NULL, NULL);
 
@@ -43,23 +42,7 @@ int main() {
 		return -1;
 	}
 
-	glViewport(0, 0, 800, 60);
-
-	// triangle vertices
-	/* clang-format off */
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.5f,  0.5f, 0.0f, 
-	};
-	/* clang-format on */
-
-	unsigned int VBO;
-	unsigned int VAO;
-
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glViewport(0, 0, 800, 600);
 
 	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
@@ -97,8 +80,37 @@ int main() {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
+	// triangle vertices
+	/* clang-format off */
+    float vertices[] = {
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,  // first Triangle
+        1, 2, 3   // second Triangle
+    };
+	/* clang-format on */
+
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s),
+	// and then configure vertex attributes(s).
 	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+				 sizeof(indices),
+				 indices,
+				 GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0,
 						  3,
 						  GL_FLOAT,
@@ -107,7 +119,19 @@ int main() {
 						  (void*)0);
 	glEnableVertexAttribArray(0);
 
+	// note that this is allowed, the call to glVertexAttribPointer registered
+	// VBO as the vertex attribute's bound vertex buffer object so afterwards we
+	// can safely unbind
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// remember: do NOT unbind the EBO while a VAO is active as the bound
+	// element buffer object IS stored in the VAO; keep the EBO bound.
+	// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally
+	// modify this VAO, but this rarely happens. Modifying other VAOs requires a
+	// call to glBindVertexArray anyways so we generally don't unbind VAOs (nor
+	// VBOs) when it's not directly necessary.
 	glBindVertexArray(0);
 
 	while(!glfwWindowShouldClose(window)) {
@@ -122,7 +146,7 @@ int main() {
 		// rendering
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
