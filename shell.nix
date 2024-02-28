bb5c86af33a03ad4a455bab82f11a7817274a906
Hudson Finn <hudson.finn@zohomail.com>
2024-02-28 12:47:36 -0500

    Shell

--- shell.nix
+++ shell.nix
@@ -2,8 +2,23 @@
 
 pkgs.mkShell {
 	nativeBuildInputs = with pkgs; [
+		eza
+		lazygit
+		# C specific things
 		cmake
 		gcc
-		glibc
+		glib
+		glfw-wayland
+		wayland
+		libxkbcommon
+		xorg.libX11
+		xorg.libXrandr
+		xorg.libXinerama
+		xorg.libXcursor
+		xorg.libXi
 	];
+	env = {
+		CC="${pkgs.gcc}/bin/gcc";
+		CXX="${pkgs.gcc}/bin/gcc";
+	};
 }
